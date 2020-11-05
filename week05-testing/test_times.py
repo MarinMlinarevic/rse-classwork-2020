d33f1ab2af1551474f5fea06fb6072b5ce301c25
MarinMlinarevic <marin.mlinarevic@outlook.com>
2020-11-05 13:01:40 +0100

    Added tests for no overlap, several intervals and touching intervals cases, and fixed discovered bugs. Answers UCL-RITS/rsd-classwork-2020#86

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -6,3 +6,24 @@ def test_given_input():
     result = compute_overlap_time(large, short)
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
+
+def test_no_overlap():
+    first = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    second = time_range("2011-01-12 10:00:00", "2011-01-12 12:00:00")
+    result = compute_overlap_time(first, second)
+    expected = []
+    assert result == expected
+
+def test_both_several_intervals():
+    first = time_range("2010-01-12 10:31:00", "2010-01-12 12:36:00", 2, 60)
+    second = time_range("2010-01-12 10:30:00", "2010-01-12 10:35:00", 2, 60)
+    result = compute_overlap_time(first, second)
+    expected = [("2010-01-12 10:31:00", "2010-01-12 10:32:00"), ("2010-01-12 10:33:00","2010-01-12 10:35:00")]
+    assert result == expected
+
+def test_one_starts_when_other_ends():
+    first = time_range("2010-01-12 10:31:00", "2010-01-12 12:36:00")
+    second = time_range("2010-01-12 12:36:00", "2010-01-12 12:40:00")
+    result = compute_overlap_time(first, second)
+    expected = [("2010-01-12 12:36:00")]
+    assert result == expected
--- week05-testing/times.py
+++ week05-testing/times.py
@@ -15,7 +15,13 @@ def compute_overlap_time(range1, range2):
     overlap_time = []
     for start1, end1 in range1:
         for start2, end2 in range2:
-            low = max(start1, start2)
-            high = min(end1, end2)
-            overlap_time.append((low, high))
+            if not (end1 < start2 or end2 < start1):
+                if start1 == end2:
+                    overlap_time.append((start1))
+                elif start2 == end1:
+                    overlap_time.append((start2))
+                else:
+                    low = max(start1, start2)
+                    high = min(end1, end2)
+                    overlap_time.append((low, high))
     return overlap_time
